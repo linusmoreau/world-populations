6d256656b8ea6e332e6f2571e1ca3812cddb4bca
Linus Moreau <linus.moreau@gmail.com>
2021-12-29 10:17:54 -0800

    Fixes

--- population.py
+++ population.py
@@ -27,7 +27,7 @@ def load_populations():
 
 
 def load_groupings():
-    with open('ideologies.txt', 'r') as f:
+    with open('ideologies.txt', 'r', encoding='utf-8') as f:
         return json.load(f)
 
 
@@ -49,7 +49,7 @@ def get_population(name: str, populations):
                  'Cote d Ivoire': 'CIV',
                  'Kyrgyzstan': 'KGZ',
                  'Guinea Bissau': 'GNB',
-                 'Saint Vinvcent and the Grenadines': 'VCT'}
+                 'Saint Vincent and the Grenadines': 'VCT'}
         try:
             tag = names[name]
             temp = populations[tag]['populations']
