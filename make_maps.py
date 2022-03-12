45697660c8965b4a88292f419e043ffc206fcc50
Linus Moreau <linus.moreau@gmail.com>
2022-03-11 20:39:09 -0800

    Change way that data is uploaded

--- make_maps.py
+++ make_maps.py
@@ -1,14 +1,15 @@
 from map_requests import get_maps
 
 
-def make_maps():
+def make_maps(start, end):
     dataset = {}
-    for year in range(1946, 2022):
-        with open("leaders_ideologies/" + str(year) + ".txt", "r") as f:
-            dataset[year] = f.readline()
+    with open("path.txt", "r") as f:
+        s = f.readline()
+    for year in range(start, end):
+        dataset[year] = s + str(year) + ".txt"
     get_maps(dataset)
 
 
 if __name__ == "__main__":
-    make_maps()
+    make_maps(1946, 2022)
 
--- map_requests.py
+++ map_requests.py
@@ -6,56 +6,71 @@ from selenium.webdriver.common.by import By
 import time
 
 
+def wait():
+    time.sleep(0.3)
+
+
 def pixel_shift(value, mini, maxi, width):
     return (value - mini) / (maxi - mini) * width
 
 
 def get_maps(dataset):
     service = Service(executable_path=EdgeChromiumDriverManager().install())
-    for year, data in dataset.items():
-        while True:
-            try:
-                get_map(year, data, service)
-                break
-            except:
-                pass
-
-
-def get_map(year, data, service):
     with webdriver.Edge(service=service) as driver:
-        driver.implicitly_wait(10)
+        driver.implicitly_wait(2)
         driver.get("https://historicalmapchart.net/world-cold-war.html")
-        time.sleep(1)
-        save_button = driver.find_element(By.ID, "downup")
-        driver.execute_script("arguments[0].scrollIntoView(true);", save_button)
-        save_button.click()
+        driver.maximize_window()
+        wait()
+        for year, data in dataset.items():
+            while True:
+                try:
+                    get_map(year, data, driver)
+                    break
+                except:
+                    pass
+        wait()
 
-        data_box = driver.find_element(By.ID, "upload-textarea")
-        data_box.send_keys(data)
 
-        time.sleep(1)
-        upload_button = driver.find_element(By.ID, "upload-config")
-        driver.execute_script("arguments[0].scrollIntoView(true);", upload_button)
-        upload_button.click()
+def get_map(year, data, driver):
+    wait()
+    save_button = driver.find_element(By.ID, "downup")
+    driver.execute_script("arguments[0].scrollIntoView(true);", save_button)
+    save_button.click()
 
-        time.sleep(1)
-        slider = driver.find_element(By.CLASS_NAME, "slider.slider-horizontal")
-        slider_button = slider.find_element(By.CLASS_NAME, "slider-handle.min-slider-handle.round")
-        driver.execute_script("arguments[0].scrollIntoView(true);", slider_button)
-        a = selenium.webdriver.ActionChains(driver)
-        a.click_and_hold(slider_button).move_by_offset(
-            pixel_shift(year, int(slider_button.get_attribute("aria-valuemin")),
-                        int(slider_button.get_attribute("aria-valuemax")), slider.size["width"]), 0).release().perform()
+    file_input = driver.find_element(By.ID, "upload-txt-inp")
+    file_input.send_keys(data)
 
-        time.sleep(1)
-        preview_button = driver.find_element(By.ID, "convert")
-        driver.execute_script("arguments[0].scrollIntoView(true);", preview_button)
-        preview_button.click()
+    wait()
+    upload_button = driver.find_element(By.ID, "upload-config")
+    driver.execute_script("arguments[0].scrollIntoView(true);", upload_button)
+    upload_button.click()
 
-        time.sleep(1)
-        download_button = driver.find_element(By.ID, "download")
-        driver.execute_script("arguments[0].scrollIntoView(true);", download_button)
-        download_button.click()
-        time.sleep(1)
+    wait()
+    slider = driver.find_element(By.CLASS_NAME, "slider.slider-horizontal")
+    slider_button = slider.find_element(By.CLASS_NAME, "slider-handle.min-slider-handle.round")
+    driver.execute_script("arguments[0].scrollIntoView(true);", slider_button)
+    a = selenium.webdriver.ActionChains(driver)
+    a.click_and_hold(slider_button).move_by_offset(
+        -pixel_shift(year, int(slider_button.get_attribute("aria-valuemin")),
+                     int(slider_button.get_attribute("aria-valuemax")), slider.size["width"]), 0).release().perform()
+    a.click_and_hold(slider_button).move_by_offset(
+        pixel_shift(year, int(slider_button.get_attribute("aria-valuemin")),
+                    int(slider_button.get_attribute("aria-valuemax")), slider.size["width"]), 0).release().perform()
+
+    wait()
+    preview_button = driver.find_element(By.ID, "convert")
+    driver.execute_script("arguments[0].scrollIntoView(true);", preview_button)
+    preview_button.click()
+
+    wait()
+    download_button = driver.find_element(By.ID, "download")
+    driver.execute_script("arguments[0].scrollIntoView(true);", download_button)
+    download_button.click()
+
+    wait()
+    edit_button = driver.find_element(By.ID, "edit")
+    driver.execute_script("arguments[0].scrollIntoView(true);", edit_button)
+    edit_button.click()
+
 
 
